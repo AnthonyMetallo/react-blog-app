746eb724cd4d266a6c87f0a598bd436b962ca0a8
AnthonyMetallo <ametallo@charlotte.edu>
2025-11-17 15:52:15 -0500

    Add login and protected routes to App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar';
 import BlogPostsPage from './pages/BlogPostsPage';
 import IndividualPostPage from './pages/IndividualPostPage';
 import ContactPage from './pages/ContactPage';
+import LoginPage from './pages/LoginPage';
+import ProtectedRoute from './components/Auth/ProtectedRoute';
 
 const App = () => {
   return (
@@ -11,9 +13,31 @@ const App = () => {
       <div className="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-50">
         <Navbar />
         <Routes>
-          <Route path="/" element={<BlogPostsPage />} />
-          <Route path="/post/:postId" element={<IndividualPostPage />} />
+          {/* Public route */}
+          <Route path="/login" element={<LoginPage />} />
+
+          {/* Protected Routes */}
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <BlogPostsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/post/:postId"
+            element={
+              <ProtectedRoute>
+                <IndividualPostPage />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Public */}
           <Route path="/contact" element={<ContactPage />} />
+
+          {/* Catch-all */}
           <Route path="*" element={<p className="text-center mt-10 text-red-500">Page not found</p>} />
         </Routes>
       </div>
