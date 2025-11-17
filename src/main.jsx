714ae95cbfc2f61873a1e02b198ac8ceacb492be
AnthonyMetallo <ametallo@charlotte.edu>
2025-11-17 15:51:23 -0500

    Wrap App component with AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { ThemeProvider } from './context/ThemeContext';
+import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* ThemeProvider wraps the entire application */}
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    {/* AuthProvider must wrap the entire app */}
+    <AuthProvider>
+      {/* ThemeProvider still works the same */}
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
