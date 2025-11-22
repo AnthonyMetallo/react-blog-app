b9b72e9ffd2a80f67f760e164c939d0b73816d38
AnthonyMetallo <ametallo@charlotte.edu>
2025-11-21 21:06:57 -0500

    Update Navbar import path in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Layout/Navbar';
+import Navbar from './components/Navbar';
 import Footer from './components/Layout/Footer';
 
 import HomePage from './pages/HomePage';
